867e7fc06e6284a80236c3338e7c3cc41105b0b0
Seddaoui Yassine <yassineseddaoui@gmail.com>
2022-06-06 22:08:33 -0400

    Adding Color Picker
    Adding a color picker to the page and a function to adapt the text color to the background

--- index.mjs
+++ index.mjs
@@ -14,10 +14,37 @@ function guess(value) {
     }
 }
 
+/**
+ * function that gives the rgb value of the HEX color
+ * @param {*} hex Hex code of the color
+ * @returns the rgb value
+ */
+function hexToRgb(hex) {
+    var result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
+    return result ? {
+      r: parseInt(result[1], 16),
+      g: parseInt(result[2], 16),
+      b: parseInt(result[3], 16)
+    } : null;
+}
 
-const input = tf.tensor2d([[1, 1, 1]]); 
+/**
+ * Function that will predict the value of the text
+ */
+function modelPredict(r, g, b) {
+    const input = tf.tensor2d([[r/255, g/255, b/255]]); 
 
-let results = model.predict(input); // Prediciton of the model
-const value = guess(results.dataSync()[0])
+    let results = model.predict(input); // Prediciton of the model
+    const value = guess(results.dataSync()[0])
 
-console.log(value);
+    return value;
+}
+
+
+document.getElementById('colorPicked').addEventListener('input', function() {
+    var rgbVal = hexToRgb(this.value);
+    var valueText = modelPredict(rgbVal.r, rgbVal.g, rgbVal.b);
+
+    document.getElementById('display').style.backgroundColor = this.value; // Changes the color of the background
+    document.getElementById('textCol').style.color = 'rgb(' + valueText*255 + ',' + valueText*255 + ',' + valueText*255 + ')';
+})
